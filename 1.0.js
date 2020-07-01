e45d4ee2272db0097ea614c6f8e8ff8b59ae16dd
Marie Pierard <pierardmarie@gmail.com>
2020-07-01 14:55:46 +0200

    structure dossiers

--- 1.0.js
+++
@@ -1,9 +0,0 @@
-let x = 5;
-let y = 8; 
-let a = 3;
-let b = 2;
-console.log(x+y);
-console.log(x-y);
-console.log(a/b);
-console.log(y*a);
-console.log(b%a);
--- 1.1.js
+++
@@ -1,4 +0,0 @@
-let x = 3;
-let y = 40;
-let age = (x+y);
-console.log(age);
